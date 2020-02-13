57ec560d61fded2850659179ce45b0ae608f65c2
Daniel Milroy <daniel.milroy@gmail.com>
2020-02-13 15:30:54 -0800

    forgot jgf

--- resource/readers/resource_reader_jgf.cpp
+++ resource/readers/resource_reader_jgf.cpp
@@ -192,9 +192,9 @@ vtx_t resource_reader_jgf_t::create_vtx (resource_graph_t &g,
 {
     size_t len = 2; // number of valid job types- should be detected automatically
     std::string rgd = "rigid", elstc = "elastic";
-    uint64_t sz = (recipe[u].size < 0) ? (uint64_t)0 : (uint64_t)recipe[u].size;
+    uint64_t sz = (fetcher.size < 0) ? (uint64_t)0 : (uint64_t)fetcher.size;
     const uint64_t resource_totals[] = {sz, sz};
-    const char *resource_types[] = {recipe[u].type.c_str (), recipe[u].type.c_str ()};
+    const char *resource_types[] = {fetcher.type.c_str (), fetcher.type.c_str ()};
     const char *job_types[] = {rgd.c_str (), elstc.c_str ()};
     planner_multi_t *plans = NULL;
     planner_t *x_checker = NULL;
