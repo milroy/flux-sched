0f648c4be72e181e76955c9dda48328a7f0adaf9
Daniel Milroy <daniel.milroy@gmail.com>
2020-02-12 23:41:42 -0800

    kicked some details

--- resource/readers/resource_reader_jgf.cpp
+++ resource/readers/resource_reader_jgf.cpp
@@ -387,7 +387,7 @@ int resource_reader_jgf_t::update_vtx_plan (vtx_t v, resource_graph_t &g,
         m_err_msg += ": plan for " + g[v].name + " is null.\n";
         goto done;
     }
-    if ( (avail = planner_multi_avail_resources_during (plans, at, dur, 
+    if ( (avail = planner_multi_avail_resources_during_by_jobtype (plans, at, dur, 
                               jobtype.c_str ())) == -1) {
         m_err_msg += __FUNCTION__;
         m_err_msg += ": planner_avail_resource_during return -1 for ";
@@ -398,7 +398,7 @@ int resource_reader_jgf_t::update_vtx_plan (vtx_t v, resource_graph_t &g,
     if (fetcher.exclusive) {
         // Update the vertex plan here (not in traverser code) so vertices
         // that the traverser won't walk still get their plans updated.
-        if ( (span = planner_add_span_by_jobtype (plans, at, dur,
+        if ( (span = planner_multi_add_span_by_jobtype (plans, at, dur,
                          static_cast<const uint64_t> (g[v].size),
                          jobtype.c_str ())) == -1) {
             m_err_msg += __FUNCTION__;
