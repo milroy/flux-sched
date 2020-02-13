3bf4d985400dd1cb8c914b48e967391920dc7f82
Daniel Milroy <daniel.milroy@gmail.com>
2020-02-12 16:52:21 -0800

    forgot declaration

--- resource/planner/planner_multi.h
+++ resource/planner/planner_multi.h
@@ -75,6 +75,7 @@ int64_t planner_multi_base_time (planner_multi_t *ctx);
 int64_t planner_multi_duration (planner_multi_t *ctx);
 size_t planner_multi_resources_len (planner_multi_t *ctx);
 const char **planner_multi_resource_types (planner_multi_t *ctx);
+const char **planner_multi_job_types (planner_multi_t *ctx);
 const uint64_t *planner_multi_resource_totals (planner_multi_t *ctx);
 int64_t planner_multi_resource_total_at (planner_multi_t *ctx, unsigned int i);
 int64_t planner_multi_resource_total_by_type (planner_multi_t *ctx,
