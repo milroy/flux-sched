c618d1dc92e754e722a06d04e4bd750c97628c6f
Daniel Milroy <daniel.milroy@gmail.com>
2020-03-26 16:11:56 -0700

    crazy ctx idea

--- resource/modules/resource_match.cpp
+++ resource/modules/resource_match.cpp
@@ -725,8 +725,12 @@ static int run_match (std::shared_ptr<resource_ctx_t> &ctx, int64_t jobid,
         }
         o << rset;
         std::cout << "parent's output: " << o.str () << std::endl;
-        //flux_close (parent_h);
-        //flux_future_destroy (f);
+        flux_close (parent_h);
+        flux_future_destroy (f);
+        if ((rc = ctx->writers->emit (o)) < 0) {
+            flux_log_error (ctx->h, "%s: writer can't emit", __FUNCTION__);
+            goto done;
+        }
     } 
     else if ((rc = ctx->writers->emit (o)) < 0) {
         flux_log_error (ctx->h, "%s: writer can't emit", __FUNCTION__);
