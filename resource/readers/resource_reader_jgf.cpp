04d6caf22d581e248f23240e96dae077c2562f96
Daniel Milroy <daniel.milroy@gmail.com>
2020-11-12 00:15:58 -0800

    out_degree bug

--- resource/readers/resource_reader_jgf.cpp
+++ resource/readers/resource_reader_jgf.cpp
@@ -959,8 +959,9 @@ int resource_reader_jgf_t::unpack_at (resource_graph_t &g,
     tie (vi, vi_end) = vertices (subg);
     for (next = vi; vi != vi_end; vi = next) {
         ++next;
-        subctmt = subg[*vi].paths.find ("containment");
-        if (subctmt == subg[*vi].paths.end ()) {
+        vtx_t tmp_v = *vi;
+        subctmt = subg[tmp_v].paths.find ("containment");
+        if (subctmt == subg[tmp_v].paths.end ()) {
             m_err_msg += __FUNCTION__;
             m_err_msg += ": containment subsystem needed for subgraph addition.\n.";
             goto done;
@@ -968,55 +969,59 @@ int resource_reader_jgf_t::unpack_at (resource_graph_t &g,
 
         g_vtx = m.by_path.find (subctmt->second);
         if (g_vtx == m.by_path.end ()) {
-            if ( (v_new = copy_vtx (g, subg, *vi)) == nullvtx)
+            if ( (v_new = copy_vtx (g, subg, tmp_v)) == nullvtx)
                 goto done;
             added_vtxs.push_back (v_new);
 
-            for (tie (ei, eie) = out_edges (*vi, subg); ei != eie; ++ei) {
-                vtx_t tgt = target (*ei, subg);
-                subctmt2 = subg[tgt].paths.find ("containment");
-                if (subctmt2 == subg[tgt].paths.end ()) {
-                    m_err_msg += __FUNCTION__;
-                    m_err_msg += ": containment subsystem needed for subgraph addition.\n.";
-                    goto done;
-                }
-                g_vtx2 = m.by_path.find (subctmt2->second);
-                if (g_vtx2 == m.by_path.end ()) {
-                    if ( (v_new2 = copy_vtx (g, subg, tgt)) == nullvtx)
+            if (out_edges (tmp_v, subg) > 0) {
+                for (tie (ei, eie) = out_edges (tmp_v, subg); ei != eie; ++ei) {
+                    vtx_t tgt = target (*ei, subg);
+                    subctmt2 = subg[tgt].paths.find ("containment");
+                    if (subctmt2 == subg[tgt].paths.end ()) {
+                        m_err_msg += __FUNCTION__;
+                        m_err_msg += ": containment subsystem needed for subgraph addition.\n.";
                         goto done;
-                    added_vtxs.push_back (v_new2);
-                }
-                else
-                    v_new2 = g_vtx2->second;
+                    }
+                    g_vtx2 = m.by_path.find (subctmt2->second);
+                    if (g_vtx2 == m.by_path.end ()) {
+                        if ( (v_new2 = copy_vtx (g, subg, tgt)) == nullvtx)
+                            goto done;
+                        added_vtxs.push_back (v_new2);
+                    }
+                    else
+                        v_new2 = g_vtx2->second;
 
-                tie (e, inserted) = add_edge (v_new, v_new2, g);
-                if (inserted == false) {
-                    errno = EPROTO;
-                    m_err_msg += __FUNCTION__;
-                    m_err_msg += ": couldn't add an edge to the graph";
-                    goto done;
-                }
-            }
-        } else {
-            for (tie (ei, eie) = out_edges (*vi, subg); ei != eie; ++ei) {
-                vtx_t tgt = target (*ei, subg);
-                subctmt3 = subg[tgt].paths.find ("containment");
-                if (subctmt3 == subg[tgt].paths.end ()) {
-                    m_err_msg += __FUNCTION__;
-                    m_err_msg += ": containment subsystem needed for subgraph addition.\n.";
-                    goto done;
-                }
-                if (m.by_path.count (subctmt3->second) == 0) {
-                    if ( (v_new = copy_vtx (g, subg, tgt)) == nullvtx)
-                        goto done;
-                    added_vtxs.push_back (v_new);
-
-                    tie (e, inserted) = add_edge (g_vtx->second, v_new, g);
+                    tie (e, inserted) = add_edge (v_new, v_new2, g);
                     if (inserted == false) {
                         errno = EPROTO;
                         m_err_msg += __FUNCTION__;
                         m_err_msg += ": couldn't add an edge to the graph";
                         goto done;
+                    }
+                }
+            }
+        } else {
+            if (out_edges (tmp_v, subg) > 0) {
+                for (tie (ei, eie) = out_edges (tmp_v, subg); ei != eie; ++ei) {
+                    vtx_t tgt = target (*ei, subg);
+                    subctmt3 = subg[tgt].paths.find ("containment");
+                    if (subctmt3 == subg[tgt].paths.end ()) {
+                        m_err_msg += __FUNCTION__;
+                        m_err_msg += ": containment subsystem needed for subgraph addition.\n.";
+                        goto done;
+                    }
+                    if (m.by_path.count (subctmt3->second) == 0) {
+                        if ( (v_new = copy_vtx (g, subg, tgt)) == nullvtx)
+                            goto done;
+                        added_vtxs.push_back (v_new);
+
+                        tie (e, inserted) = add_edge (g_vtx->second, v_new, g);
+                        if (inserted == false) {
+                            errno = EPROTO;
+                            m_err_msg += __FUNCTION__;
+                            m_err_msg += ": couldn't add an edge to the graph";
+                            goto done;
+                        }
                     }
                 }
             }
