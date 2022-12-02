3fb8bbc605bb9450dda1a55c3a48e597794a5195
Daniel Milroy <daniel.milroy@gmail.com>
2022-12-02 00:41:25 -0800

    schema: printouts

--- resource/schema/sched_data.cpp
+++ resource/schema/sched_data.cpp
@@ -9,6 +9,7 @@
 \*****************************************************************************/
 
 #include "resource/schema/sched_data.hpp"
+#include <iostream>
 
 namespace Flux {
 namespace resource_model {
@@ -27,8 +28,11 @@ schedule_t::schedule_t (const schedule_t &o)
         reservations.emplace (reserv_it.first, reserv_it.second);
     }
 
+    std::cout << "SCHED_T COPY CTOR\n";
     if (o.plans) {
-        plans = o.plans;
+        if (!plans)
+            plans = new planner_t ();
+        plans = new planner_t (*(o.plans));
     }
 }
 
@@ -40,9 +44,14 @@ schedule_t &schedule_t::operator= (const schedule_t &o)
     for (auto const &reserv_it : o.reservations) {
         reservations.emplace (reserv_it.first, reserv_it.second);
     }
+
+    std::cout << "SCHED_T ASSIGN CTOR\n";
     
     if (o.plans) {
-        plans = o.plans;
+        if (!plans)
+            plans = new planner_t ();
+        *plans = *(o.plans);
+        std::cout << "SCHED_T ASSIGN PLANS\n";
     }
     return *this;
 }
@@ -51,6 +60,9 @@ schedule_t::~schedule_t ()
 {
     if (plans)
         planner_destroy (&plans);
+
+    allocations.clear ();
+    reservations.clear ();
 }
 
 } // resource_model
