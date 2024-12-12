9fc214df2be6b6b1d024a853b9d47da18bdddcdb
Daniel Milroy <daniel.milroy@gmail.com>
2024-12-12 01:15:45 -0800

    evaluators: add extraction tests in unit tests
    Problem: the evaluator unit tests do not support extraction and the new
    options.
    
    Add support in the tests and test cases for the new options.

--- resource/evaluators/test/expr_eval_test01.cpp
+++ resource/evaluators/test/expr_eval_test01.cpp
@@ -25,19 +25,34 @@ using namespace Flux::resource_model;
 
 class expr_eval_test_target_t : public expr_eval_target_base_t {
    public:
-    virtual int validate (const std::string &p, const std::string &x) const;
+    virtual int validate (const std::string &p, const std::string &x) const override;
 
-    virtual int evaluate (const std::string &p, const std::string &x, bool &result) const;
+    virtual int evaluate (const std::string &p, const std::string &x, bool &result) const override;
+
+    virtual int extract (const std::string &p,
+                         const std::string &x,
+                         unsigned long &jobid,
+                         bool &agfilter) const override;
 };
 
 int expr_eval_test_target_t::validate (const std::string &p, const std::string &x) const
 {
-    if (p != "status" && p != "sched-now")
+    if (p != "status" && p != "sched-now" && p != "jobid-alloc" && p != "agfilter")
         return -1;
     if (p == "status" && (x != "up" && x != "down"))
         return -1;
     if (p == "sched-now" && (x != "allocated" && x != "free"))
         return -1;
+    if (p == "jobid-alloc") {
+        try {
+            std::stoul (x);
+        } catch (std::out_of_range) {
+            return -1;
+        }
+    }
+    if (p == "agfilter" && (x != "true")) {
+        return -1;
+    }
     return 0;
 }
 
@@ -50,6 +65,25 @@ int expr_eval_test_target_t::evaluate (const std::string &p,
         return rc;
     result = true;
     return rc;
+}
+
+int expr_eval_test_target_t::extract (const std::string &p,
+                                      const std::string &x,
+                                      unsigned long &jobid,
+                                      bool &agfilter) const
+{
+    if (validate (p, x) < 0)
+        return -1;
+    if (p == "jobid-alloc") {
+        if (x != "") {
+            jobid = std::stoul (x);
+        } else {
+            jobid = 0;
+        }
+    } else if (p == "agfilter") {
+        agfilter = true;
+    }
+    return 0;
 }
 
 void build_simple_expr (std::vector<std::string> &expr_vector)
@@ -79,6 +113,9 @@ void build_simple_expr (std::vector<std::string> &expr_vector)
     expr_vector.push_back ("sched-now=free status=down");
     expr_vector.push_back ("sched-now=free and status=down");
     expr_vector.push_back ("sched-now=free  or          status=down");
+    expr_vector.push_back ("agfilter=true");
+    expr_vector.push_back ("jobid-alloc=1");
+    expr_vector.push_back ("jobid-alloc=1 and agfilter=true");
 }
 
 void build_paren_expr (std::vector<std::string> &expr_vector)
@@ -121,6 +158,15 @@ void build_invalid_expr (std::vector<std::string> &expr_vector)
         "(sched-now=free)(status=down)");
     expr_vector.push_back ("(status=up and sched-now=allocated))");
     expr_vector.push_back ("(status=up and sched-now=allocated)))");
+    expr_vector.push_back ("jobid-alloc=18446744073709551616");
+}
+
+void build_extract_expr (std::vector<std::string> &expr_vector)
+{
+    expr_vector.push_back ("jobid-alloc=1");
+    expr_vector.push_back ("jobid-alloc=10");
+    expr_vector.push_back ("jobid-alloc=10 and agfilter=true");
+    expr_vector.push_back ("agfilter=true");
 }
 
 void test_validation (std::vector<std::string> &expr_vector,
@@ -159,12 +205,46 @@ void test_evaluation (std::vector<std::string> &expr_vector,
     }
 }
 
+void test_extraction (std::vector<std::string> &expr_vector,
+                      std::vector<unsigned long> &jobids,
+                      std::vector<bool> &agfilters,
+                      const std::string &label,
+                      bool must_success)
+{
+    int rc = 0;
+    int i = 0;
+    bool expected = false;
+    bool expected_jobid = false;
+    bool expected_agfilter = false;
+    bool agfilter = false;
+    unsigned long jobid = 0;
+    Flux::resource_model::expr_eval_api_t evaluator;
+    expr_eval_test_target_t expr_eval_test_target;
+
+    for (i = 0; i < expr_vector.size (); ++i) {
+        agfilter = false;
+        jobid = 0;
+        rc = evaluator.extract (expr_vector.at (i), expr_eval_test_target, jobid, agfilter);
+        expected = must_success ? rc == 0 : rc < 0;
+        expected_jobid = (jobids.at (i) == jobid);
+        expected_agfilter = (agfilters.at (i) == agfilter);
+
+        ok (expected && expected_jobid && expected_agfilter,
+            "%s: ^%s$",
+            label.c_str (),
+            expr_vector.at (i).c_str ());
+    }
+}
+
 int main (int argc, char *argv[])
 {
     size_t ntests = 0;
     std::vector<std::string> expr_vector1;
     std::vector<std::string> expr_vector2;
     std::vector<std::string> expr_vector3;
+    std::vector<std::string> expr_vector4;
+    std::vector<unsigned long> extr_vector_jobids{1, 10, 10, 0};
+    std::vector<bool> extr_vector_ag{false, false, true, true};
 
     build_simple_expr (expr_vector1);
 
@@ -172,10 +252,11 @@ int main (int argc, char *argv[])
 
     build_invalid_expr (expr_vector3);
 
-    ntests = expr_vector1.size () + expr_vector2.size () + expr_vector3.size ();
-    ;
+    build_extract_expr (expr_vector4);
 
-    plan (2 * ntests);
+    ntests = expr_vector1.size () + expr_vector2.size () + expr_vector3.size ();
+
+    plan (2 * ntests + expr_vector4.size ());
 
     test_validation (expr_vector1, "validates simple expr", true);
 
@@ -188,6 +269,12 @@ int main (int argc, char *argv[])
     test_validation (expr_vector3, "invalidates malformed expr", false);
 
     test_evaluation (expr_vector3, "expectedly evaluates malformed", false);
+
+    test_extraction (expr_vector4,
+                     extr_vector_jobids,
+                     extr_vector_ag,
+                     "extracts as expected",
+                     true);
 
     done_testing ();
 
