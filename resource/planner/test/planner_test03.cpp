4ff9ce8419b1df6a8756d5c57b2cf9dd2b90b5fc
Daniel Milroy <daniel.milroy@gmail.com>
2025-07-04 14:26:45 -0700

    planner2: update unit tests

--- resource/planner/test/planner_test03.cpp
+++ resource/planner/test/planner_test03.cpp
@@ -92,6 +92,9 @@ static int test_add_remove ()
     auto mc = plan2->m_multi_container.get<time_count> ().lower_bound (6);
     std::cout << "time: " << mc->at_time << " occupied: " << n_res - mc->free_ct << " free resources: " << mc->free_ct << " End? " << (mc == plan2->m_multi_container.get<time_count> ().end ()) <<  "\n";
 
+    auto mc2 = plan2->m_multi_container.get<time_count> ().lower_bound (boost::make_tuple (0, 6), earliest_free ());
+    std::cout << "time: " << mc2->at_time << " occupied: " << n_res - mc2->free_ct << " free resources: " << mc2->free_ct << " End? " << (mc2 == plan2->m_multi_container.get<time_count> ().end ()) <<  "\n";
+
     std::cout << "Multi_index span size: " << plan2->m_span_lookup.size () << std::endl;
     std::cout << "Multi_index container size: " << plan2->m_multi_container.size () << std::endl;
 
